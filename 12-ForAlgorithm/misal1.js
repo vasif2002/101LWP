5fae40d04e893fb9faea4e92b40c4cee707de129
vasif ejderov <126759414+vasif2002@users.noreply.github.com>
2023-04-12 09:47:48 +0400

    Add files via upload

---
+++ 12-ForAlgorithm/misal1.js
@@ -0,0 +1,32 @@
+sampleNews= `As software developers,the natural tendency is tostart developing applications based on your own handson 
+eperience and knoedge riht awy.howevr,overtme isues 
+ the aplication aries ,adaptaions to changs, andnew featres.`
+
+// let say=0;
+// let bosluq=" ";
+// for(let i=0 ;i<sampleNews .length;i++){
+// if( sampleNews[i]===bosluq){
+//     say++; 
+// }
+// }
+// console.log( 'bosluqlarin sayi' ,say);
+
+
+
+// VEDGUL
+
+let say1=0;
+let say2=0
+let nokte=".";
+let vergul=",";
+for(let i=0 ;i<sampleNews .length;i++){
+if( sampleNews[i]===nokte){
+    say1++; 
+}
+
+if( sampleNews[i]===vergul){
+   say2++ 
+}
+
+}
+console.log( 'nokte ' ,say1,  'vergul' ,say2);
